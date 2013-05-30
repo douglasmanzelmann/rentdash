3353b67d2f1d8223cfacdcd9ca52b2bab9fcea19
Douglas Manzelmann <dougamanzelmann@gmail.com>
2013-05-30 14:30:12 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rentdash::Application.config.secret_token = '46698113315c1637424183d28660708854870a9411fe8d557e987e281e3b57359d2e4ea762cf8c99fc3a9800a3dd344b121efdce05cd61f90157ded7028cd18a'
